7e2f12d98aa37f751f95ca5214428570fdb3b051
Fatahillah Satria Bima Seno <55565303+fathsbs@users.noreply.github.com>
2022-01-05 05:38:41 +0700

    Add files via upload

---
+++ screaming_repeating.py
@@ -0,0 +1,9 @@
+# take value of how many times mom must tell me
+times = input('How many times do i have to tell you? ')
+
+# change str value to int value
+times = int(times)
+
+#for loop mom tells me clean up my room
+for time in range(times):
+    print('CLEAN UP YOUR ROOM!')
